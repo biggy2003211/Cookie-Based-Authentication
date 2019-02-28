61bca89dd4daebdc0b04797e2ea19b9e37bc347a
LeonBing <biggy2003211@gmail.com>
2019-02-28 15:15:15 +0800

    完成_SignInViewModel類別接收傳回的帳號和密碼

---
+++ Cookie-Based-Authentication/ViewModel/SignInViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cookie_Based_Authentication.ViewModel
+{
+    // ViewModel 的功能是接收前端傳回的資料，或回傳資料給前端，作為內部和外部溝通的橋樑。
+    public class SignInViewModel
+    {
+        //帳號
+        public string UserId { get; set; }
+        //密碼
+        public string Password { get; set; }
+    }
+}
