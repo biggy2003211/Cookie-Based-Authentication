b587637efe1fcf6c39f69f8da9fd8ad970241355
LeonBing <biggy2003211@gmail.com>
2019-02-28 14:58:48 +0800

    完成_Identity列舉(Enum)管理使用者身分

---
+++ Cookie-Based-Authentication/Models/Identity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Cookie_Based_Authentication.Models
+{
+    public enum Identity
+    {
+        [Description("管理者")]
+        Admin = 1,
+
+        [Description("一般使用者")]
+        User = 2,
+    }
+}
