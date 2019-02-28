c2c6642b22fc5a380715e0a9282c9ac25248698b
LeonBing <biggy2003211@gmail.com>
2019-02-28 15:01:11 +0800

    完成_User類別定義要存入Cookie的資訊

---
+++ Cookie-Based-Authentication/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cookie_Based_Authentication.Models
+{
+    public class User
+    {
+        //流水號
+        public int Id { get; set; }
+        //帳號
+        public string UserId { get; set; }
+        //名稱
+        public string UserName { get; set; }
+        //身分
+        public Identity Identity { get; set; }
+    }
+}
