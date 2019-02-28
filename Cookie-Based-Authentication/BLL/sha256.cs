1f023af10e4a8ede61b6634727fca2f342cefd90
LeonBing <biggy2003211@gmail.com>
2019-02-28 17:56:29 +0800

    完成_sha256雜湊加密機制

---
+++ Cookie-Based-Authentication/BLL/sha256.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace Cookie_Based_Authentication.BLL
+{
+    public class sha256
+    {
+        /// <summary>
+        /// 使用 sha256 雜湊加密
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
+        public string Encryption_sha256(string password)
+        {
+            //建立一個SHA256
+            SHA256 sha256 = new SHA256CryptoServiceProvider();
+            //將字串轉為Byte[]
+            Console.WriteLine("請輸入密碼:");
+            byte[] source = Encoding.Default.GetBytes(password);
+            //進行SHA256加密
+            byte[] crypto = sha256.ComputeHash(source);
+            //把加密後的字串從Byte[]轉為字串
+            string result = Convert.ToBase64String(crypto);
+            return result;
+        }
+    }
+}
